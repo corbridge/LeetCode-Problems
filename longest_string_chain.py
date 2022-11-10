0363687a490307031236200082e45b59cfa3b481
Cornelio Posadas <109637167+CornelioEmbedded@users.noreply.github.com>
2022-11-09 19:41:11 -0600

    work on remove duplicates in string

---
+++ longest_string_chain.py
@@ -0,0 +1,12 @@
+def removeDuplicates(s):
+  stack = []
+  
+  for char in s:
+    if len(stack) == 0:
+      stack.append(char)
+     else:
+      if stack[-1] == char:
+        stack.pop()
+      else:
+        stack.append(char)
+  return "".join(stack)
